3884e8298189e8ae4923c0b1cb635877af7e5234
vibhaas <vibhaas@gmail.com>
2024-11-02 12:02:30 +0530

    upd: added .env files for mysql.connector

--- server/app.py
+++ server/app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from routes.add_deck import add_deck_bp
+from routes.delete_deck import delete_deck_bp
+
 
 app = Flask(__name__)
 
--- server/config.py
+++ server/config.py
@@ -1,1 +1,21 @@
-# pass
+"""Flask APP configurations"""
+from dotenv import load_dotenv
+import os
+from urllib.parse import quote_plus
+import mysql.connector
+
+load_dotenv()
+
+class Config:
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))  # URL-encode the password
+    DB_HOST = os.getenv("DB_HOST")
+    DB_NAME = os.getenv("DB_NAME")
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=Config.DB_HOST,
+        user=Config.DB_USER,
+        password=Config.DB_PASSWORD,
+        database=Config.DB_NAME
+    )
